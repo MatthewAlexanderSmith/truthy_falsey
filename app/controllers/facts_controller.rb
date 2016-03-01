567c5774b1001a66cb4716713ad7281a19ebb9b3
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-01 18:45:06 -0500

    add edit and update actions to fact_controller, create basic views

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,4 +1,6 @@
 class FactsController < ApplicationController
+  before_action :load_fact, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @facts = Fact.all
@@ -18,7 +20,7 @@ class FactsController < ApplicationController
     
     @fact.user = current_user
     if @fact.save
-      redirect_to fact_path(@fact), notice: 'Pledge successfully updated.'
+      redirect_to fact_path(@fact), notice: 'Fact successfully created.'
     else
       render :new
     end
@@ -27,8 +29,13 @@ class FactsController < ApplicationController
   def edit
   end
 
-
   def update
+    @fact.update_attributes(fact_params)
+    if @fact.save
+      redirect_to fact_path(@fact), notice: 'Fact successfully created.'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -38,6 +45,10 @@ class FactsController < ApplicationController
 
   def fact_params
     params.require(:fact).permit(:body, :user_id)
+  end
+
+  def load_fact
+    @fact = Fact.find(params[:id])
   end
 
 end
