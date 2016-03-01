d67d53cb92248ef25067fdbcba29b198162a3a3e
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-03-01 16:50:22 -0500

    verify basic associations tweak gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(display_name: "test_user", email: "msmithros@hotmail.com", password: "testpassword", password_confirmation:
+    "testpassword", )
+
+Comment.create(user: User.first)
+
+Fact.create(user: User.first)
+
+Evidence.create(user: User.first, fact: Fact.first)
+
+Vote.create(user: User.first, evidence: Evidence.first)
