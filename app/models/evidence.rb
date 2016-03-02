27890e8926bb23c6e48092ff03429601e7aaac5e
Chris Staikos <cstaikos@gmail.com>
2016-03-02 10:26:16 -0500

    Show upvotes/downvotes on evidence on fact show view

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -2,4 +2,13 @@ class Evidence < ActiveRecord::Base
   belongs_to :user
   belongs_to :fact
   has_many :votes
+
+
+  def upvotes
+    self.votes.where(upvote: true).count
+  end
+
+  def downvotes
+    self.votes.where(upvote: false).count
+  end
 end
